54044c03b8b3f259a252027464d43ec05735217c
Tony Eneh <aeneh90@gmail.com>
2020-11-09 04:30:12 +0100

    Fix: remove UI Components from configurator state

--- src/Configurator/index.js
+++ src/Configurator/index.js
@@ -12,6 +12,21 @@ import { Star } from "./Star";
 import { Trapezium } from "./Trapezium";
 import { Triangle } from "./Triangle";
 
+// put all shapes in an for easier rendering of the UI dynamically
+const configInfo = {
+  Circle,
+  Ellipse,
+  Square,
+  Rectangle,
+  Kite,
+  Parallelogram,
+  Polygon,
+  Rhombus,
+  Star,
+  Trapezium,
+  Triangle,
+};
+
 export class Configurator extends Component {
   constructor(props) {
     super(props);
@@ -24,21 +39,6 @@ export class Configurator extends Component {
   // configInfo is an object which has the above shapes as properties whose
   state = {
     shape: "Circle",
-
-    // for UI rendering
-    configInfo: {
-      Circle,
-      Ellipse,
-      Square,
-      Rectangle,
-      Kite,
-      Parallelogram,
-      Polygon,
-      Rhombus,
-      Star,
-      Trapezium,
-      Triangle,
-    },
 
     // state objects to hold the values of the inputs
     Circle: {},
@@ -80,14 +80,14 @@ export class Configurator extends Component {
           value={this.state.shape}
           onChange={this.handleShapeChange}
         >
-          {Object.keys(this.state.configInfo).map((shape, id) => (
+          {Object.keys(configInfo).map((shape, id) => (
             <option key={id}>{shape}</option>
           ))}
         </select>
         {/* render the inputs peculiar to the current shape inside the fieldset below */}
         {/* pass in a change handler for any change in inputs */}
         <fieldset>
-          {React.createElement(this.state.configInfo[this.state.shape], {
+          {React.createElement(configInfo[this.state.shape], {
             onChange: this.handleShapeDetailsChange,
           })}
         </fieldset>
