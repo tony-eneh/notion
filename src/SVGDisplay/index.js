58d2d080ff32fe631dbaf1014dc501b578fdb25c
Tony Eneh <aeneh90@gmail.com>
2020-11-10 15:20:38 +0100

    Feat: display demo path in svg area

--- src/SVGDisplay/index.js
+++ src/SVGDisplay/index.js
@@ -1,9 +1,11 @@
 import "./svgdisplay.css";
 
-export function SVGDisplay() {
+export function SVGDisplay({ d }) {
+  console.log(d);
   return (
     <svg viewBox="0 0 500 500">
-      <rect></rect>
+      <rect width="500" height="500" fill="#232323" />
+      <path d={d} fill="pink" stroke="lavenderblush" strokeWidth="10" />
     </svg>
   );
 }
