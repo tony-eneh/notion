cfb401c53219c52ab0d09773888124de0e8839ce
Tony Eneh <aeneh90@gmail.com>
2020-11-10 16:11:26 +0100

    Fix: move up app state to App component

--- src/App.js
+++ src/App.js
@@ -4,18 +4,55 @@ import { Header } from "./Header";
 import { Configurator } from "./Configurator";
 import { SVGDisplay } from "./SVGDisplay";
 import { Component } from "react";
+import { getD } from "./Configurator/Circle";
+import { CENTER } from "./constants";
+import { configFields, defaultValues } from "./Configurator/Shapes";
 
 class App extends Component {
-  handleConfigChange(config) {
-    console.log("config changed", config);
+  constructor(props) {
+    super(props);
+    this.state = { shape: "Circle", details: {} };
+    this.handleShape = this.handleShape.bind(this);
+    this.handleDetails = this.handleDetails.bind(this);
+  }
+
+  initializeInputs() {
+    // get the values for the currently selected shape and set state with it
+    this.setState({ details: defaultValues[this.state.shape] });
+  }
+
+  handleShape(shape) {
+    // fetch the default values for this shape
+    // then set both the shape and its details to state
+    const details = defaultValues[shape];
+    this.setState({ shape, details });
+  }
+
+  handleDetails(detail) {
+    // create a new details object with new value merged in
+    const details = { ...this.state.details, ...detail };
+    this.setState({ details });
+  }
+
+  componentDidMount() {
+    // populate input elements with default values
+    this.initializeInputs();
+  }
+
+  componentDidUpdate() {
+    console.log(this.state);
   }
 
   render() {
     return (
       <div className="App">
         <Header />
-        <Configurator onChange={this.handleConfigChange} />
-        <SVGDisplay />
+        <Configurator
+          setShape={this.handleShape}
+          setDetails={this.handleDetails}
+          data={this.state}
+        />
+        <SVGDisplay d={this.state.d} />
       </div>
     );
   }
--- src/Configurator/index.js
+++ src/Configurator/index.js
@@ -1,62 +1,33 @@
 import React, { Component } from "react";
 import "./configurator.css";
-import { configFields, defaultValues } from "./Shapes";
+import { configFields } from "./Shapes";
 
 // put all shapes in an for easier rendering of the UI dynamicall
 
-export class Configurator extends Component {
-  constructor(props) {
-    super(props);
-    this.fireConfigChange = props.onChange.bind(this);
-    this.handleShapeChange = this.handleShapeChange.bind(this);
-    this.handleShapeDetailsChange = this.handleShapeDetailsChange.bind(this);
-  }
-
-  state = {
-    shape: "Circle",
-    // state objects to hold the values of the inputs
-    ...defaultValues,
-  };
-
-  handleShapeChange(e) {
-    this.setState({ shape: e.target.value });
-  }
-
-  handleShapeDetailsChange(e) {
-    const currentShape = this.state[this.state.shape];
-
-    // ensured type consistency by casting value to number before assigning
-    currentShape[e.target.name] = +e.target.value;
-
-    this.setState({ [currentShape]: currentShape });
-    console.log("you changed a value");
-    console.log("target element name", e.target.name);
-  }
-
-  componentDidUpdate() {
-    this.fireConfigChange(this.state);
-  }
-
-  render() {
-    return (
-      <form className="configurator">
-        <select
-          type="select"
-          value={this.state.shape}
-          onChange={this.handleShapeChange}
-        >
-          {Object.keys(configFields).map((shape, id) => (
-            <option key={id}>{shape}</option>
-          ))}
-        </select>
-        {/* render the inputs peculiar to the current shape inside the fieldset below */}
-        {/* pass in a change handler for any change in inputs */}
-        <fieldset>
-          {React.createElement(configFields[this.state.shape], {
-            onChange: this.handleShapeDetailsChange,
-          })}
-        </fieldset>
-      </form>
-    );
-  }
-}
+export const Configurator = () => {
+  return (
+    <form className="configurator">
+      <select
+        type="select"
+        value={this.props.data.shape}
+        onChange={(e) => this.props.setShape(e.target.value)}
+      >
+        {Object.keys(configFields).map((shape, id) => (
+          <option key={id}>{shape}</option>
+        ))}
+      </select>
+      {/* render the inputs peculiar to the current shape inside the fieldset below */}
+      {/* pass in a change handler for any change in inputs */}
+      <fieldset>
+        {React.createElement(configFields[this.props.data.shape], {
+          onChange: (e) => {
+            // create a detail object which identifies what changed and the new value
+            // use the callback from parent to register the change
+            const detail = { [e.target.name]: +e.target.value };
+            this.props.setDetails(detail);
+          },
+        })}
+      </fieldset>
+    </form>
+  );
+};
